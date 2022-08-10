83974dda0a997894087e317ed11d82e7b160a42c
LoPij <lopihavefun@gmail.com>
2022-08-10 20:18:08 +0300

    import ping

--- modules/debug.py
+++ modules/debug.py
@@ -5,6 +5,8 @@ from typing import Optional
 import lightbulb
 from lightbulb import PrefixCommand, SlashCommand
 
+import time
+
 import glob
 import settings
 
@@ -14,7 +16,30 @@ plugin = lightbulb.Plugin("debug")
 @lightbulb.command("ping", "See if bot alive")
 @lightbulb.implements(SlashCommand, PrefixCommand)
 async def ping(ctx: lightbulb.Context) -> None:
-    await ctx.respond("Pong!")
+    osu_api_test_link = (
+        "https://osu.ppy.sh/api/"
+        f"get_beatmaps?k={settings.OSU_API_KEY}"
+        f"&b=1"
+    )
+
+    time_start = time.time()
+    r = await glob.osu.get(osu_api_test_link)
+    time_end = time.time()
+
+    diff = int((time_end - time_start) * 1000)
+
+    osu_text = f"osu!api: **{diff}ms** `{r.status}`\n"
+
+    secret_api_test_link = settings.SUPER_SECRET_API_LINK.format(100)
+    time_start = time.time()
+    r = await glob.osu.get(secret_api_test_link)
+    time_end = time.time()
+
+    diff = int((time_end - time_start) * 1000)
+
+    secret_api_text = f"secret api: **{diff}**ms `{r.status}`\n"
+
+    await ctx.respond("Pong!\n" + osu_text + secret_api_text)
 
 def load(bot: lightbulb.BotApp):
     print("Loaded debug plugin")
